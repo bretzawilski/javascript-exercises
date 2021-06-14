d2dc51d4478fc0deb22b8dc7563d69749dbc7034
Bret Zawilski <bret.zawilski@gmail.com>
2021-06-14 21:23:56 +0100

    Fibonacci Solved

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (pos) {
+  if (pos < 1) {
+    return "OOPS";
+  }
+  let fib = [];
+  for (i = 0; i < pos; i++) {
+    if (fib.length < 1) {
+      fib[i] = 1;
+    } else if (fib.length < 2) {
+      fib[i] = 1;
+    } else {
+      fib[i] = (fib[i - 2] + fib[i - 1]);
+    }
+  }
+  return fib[pos - 1];
 };
+debugger
+fibonacci(4);
 
 module.exports = fibonacci;
